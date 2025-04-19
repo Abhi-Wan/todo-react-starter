3c4db104b335b84bc2c911115dd830be74cda296
Abhinav <Abhinav.42.Singh@gmail.com>
2025-04-18 17:00:53 -0700

    Functionality to add task to list via form submission

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,20 @@
-function Form() {
+import { useState } from "react";
+
+function Form(props) {
+  const [name, setName] = useState("");
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
-    alert("Hello, world");
+    if (name.length > 0) {
+      props.addTask(name);
+    }
+    setName("");
   }
+
   return (
     <form onSubmit={handleSubmit}>
       <h2 className="label-wrapper">
@@ -16,11 +28,13 @@ function Form() {
         className="input input__lg"
         name="text"
         autoComplete="off"
+        value={name}
+        onChange={handleChange}
       />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
+      <button type="submit" className="btn btn__primary btn__lg">
+        Add
+      </button>
+    </form>
   );
 }
 
